8dd9ae4b29d26607d77871de3a4ad43ff0e031a7
Kr3m <kremisoski@gmail.com>
2023-02-11 13:37:44 -0500

    fixed to build qvm on Linux

--- etc/lcc.c
+++ etc/lcc.c
@@ -232,7 +232,7 @@ static int _spawnvp(int mode, const char *cmdname, const char *const argv[]) {
 		fprintf(stderr, "%s: no more processes\n", progname);
 		return 100;
 	case 0:
-		execv(cmdname, (char **)argv);
+		execvp(cmdname, (char **)argv);
 		fprintf(stderr, "%s: ", progname);
 		perror(cmdname);
 		fflush(stdout);
